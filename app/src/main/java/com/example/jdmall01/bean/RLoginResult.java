7735ef673d332e8b74f12a72ddb4fac838e341a8
shmily <woshixuyihe@126.com>
2019-06-19 23:09:09 +0800

    complete regist function

--- app/src/main/java/com/example/jdmall01/bean/RLoginResult.java
+++ app/src/main/java/com/example/jdmall01/bean/RLoginResult.java
@@ -2,10 +2,58 @@ package com.example.jdmall01.bean;
 
 public class RLoginResult {
 
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getUserIcon() {
+        return userIcon;
+    }
+
+    public int getWaitPayCount() {
+        return waitPayCount;
+    }
+
+    public int getWaitReceiveCount() {
+        return waitReceiveCount;
+    }
+
+    public int getUserLevel() {
+        return userLevel;
+    }
+
     private long id;    //用户id
     private String username;    //用户名
     private String userIcon;    //头像路径
     private int waitPayCount;   //待付款数
     private int waitReceiveCount;   //待收货数
     private int userLevel;  //1注册会员  2铜牌会员   3银牌会员   4金牌会员   5钻石会员
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setUserIcon(String userIcon) {
+        this.userIcon = userIcon;
+    }
+
+    public void setWaitPayCount(int waitPayCount) {
+        this.waitPayCount = waitPayCount;
+    }
+
+    public void setWaitReceiveCount(int waitReceiveCount) {
+        this.waitReceiveCount = waitReceiveCount;
+    }
+
+    public void setUserLevel(int userLevel) {
+        this.userLevel = userLevel;
+    }
 }
--- app/src/main/java/com/example/jdmall01/constant/IdiyMessage.java
+++ app/src/main/java/com/example/jdmall01/constant/IdiyMessage.java
@@ -4,4 +4,7 @@ public class IdiyMessage {
 
     public static final int LOGIN_ACTION = 1;
     public static final int LOGIN_ACTION_RESULT = 2;
+
+    public static final int REGIST_ACTION = 3;
+    public static final int REGIST_ACTION_RESULT = 4;
 }
