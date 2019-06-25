6b8fdb36c87d997684cd0faaaf35233165d8c79b
Geek981108 <shmily150617@gmail.com>
2019-06-25 21:00:03 +0800

    complete Userdao

--- app/src/main/java/com/example/jdmall01/db/UserDao.java
+++ app/src/main/java/com/example/jdmall01/db/UserDao.java
@@ -1,5 +1,38 @@
 package com.example.jdmall01.db;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.jdmall01.constant.DbConst;
+
 public class UserDao {
+
+    //1.数据库表中只能有一个用户
+
+    private DbOpenHelper mHelper;
+
+    public UserDao(Context c) {
+        mHelper = new DbOpenHelper(c);
+    }
+
+    //保存数据库
+    public boolean saveUser(String name, String pwd) {
+
+        SQLiteDatabase db = mHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(DbConst.DB_NAME, name);
+        values.put(DbConst._PWD, pwd);
+        long insertId = db.insert(DbConst.USER_TABLE, null, values);
+        return insertId != -1;
+
+    }
+
+    //清空数据表
+    public void clearUsers() {
+
+        SQLiteDatabase db = mHelper.getWritableDatabase();
+        db.delete(DbConst.USER_TABLE, null, null);
+    }
 
 }
