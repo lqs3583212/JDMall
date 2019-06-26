882eee7bf3ec6ca10566ebf34956f0074ad47b68
Geek981108 <shmily150617@gmail.com>
2019-06-26 23:59:04 +0800

    solve some bugs.....

--- app/src/main/java/com/example/jdmall01/activity/LoginActivity.java
+++ app/src/main/java/com/example/jdmall01/activity/LoginActivity.java
@@ -92,8 +92,8 @@ public class LoginActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        initUI();
         initController();
+        initUI();
 
 
     }
