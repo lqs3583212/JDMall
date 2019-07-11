9b7e9c87a50fb00d313bd01e370fccf096bad29c
geek981108 <shmily150617@gmail.com>
2019-07-11 23:03:22 +0800

    add some Layout and fix bugs

--- app/src/main/java/com/example/jdmall01/fragment/MyJDFragment.java
+++ app/src/main/java/com/example/jdmall01/fragment/MyJDFragment.java
@@ -14,7 +14,7 @@ public class MyJDFragment extends BaseFragment implements View.OnClickListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_myjd, container, false);
     }
 
     @Override
